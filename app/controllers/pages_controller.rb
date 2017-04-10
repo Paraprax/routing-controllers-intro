22ff49f6c893f18dc9f0ad5c2e52fe6c7e79a7fe
Paraprax <parkerjohnston28@gmail.com>
2017-04-10 16:28:49 -0400

    adding route helpers to controller page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,7 +12,7 @@ class PagesController < ApplicationController
   def contest
     @header = "This is the contest page"
     flash[:notice] = "Sorry, the contest has ended"
-    redirect_to "/welcome"
+    redirect_to welcome_path
   end
 
   def kitten
@@ -24,6 +24,20 @@ class PagesController < ApplicationController
   def set_kitten_url
     requested_size = params[:size]
     @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
+
+  def secret
+  end
+
+  def secrets
+      #speak friend and enter
+      if
+      params[:magic_word] == "mellon"
+      render :secrets
+    else
+      redirect_to welcome_path
+      flash[:alert] = "Some things are better left unknown."
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#welcome'
+
   get '/welcome' => 'pages#welcome'
 
   get '/about' => 'pages#about'
 
   get '/contest' => 'pages#contest'
 
-  get '/kitten/:size' => 'pages#kitten'
+  get '/kitten/:size' => 'pages#kitten', as: 'kitten'
 
-  get 'kittens/:size' => 'pages#kittens'
+  get 'kittens/:size' => 'pages#kittens', as: 'kittens'
+
+  get '/secrets' => 'pages#secret', as: 'secret'
+
+  get '/secrets/:magic_word' => 'pages#secrets', as: 'secrets'
 
 end
